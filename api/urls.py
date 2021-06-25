39ee0b0a764e245a477370c45c3d3c17296deb6a
Vinicius <vateiixeira@gmail.com>
2021-06-24 23:08:16 -0300

    manage urls to same folder api

---
+++ api/urls.py
@@ -0,0 +1,26 @@
+from rest_framework import routers
+from api import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls import url
+from rest_framework import permissions
+from rest_framework.routers import DefaultRouter
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="MagPy API",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+router = DefaultRouter()
+
+router.register(r'projects', views.ProjectViewSet, basename='projects')
+
+urlpatterns = [
+    url(r'^swagger$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
+
+urlpatterns += router.urls
--- magpy/urls.py
+++ magpy/urls.py
@@ -19,10 +19,7 @@ from rest_framework import routers
 
 from api import views
 
-router = routers.SimpleRouter()
-router.register(r'projects', views.ProjectViewSet)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include('api.urls')),
 ]
